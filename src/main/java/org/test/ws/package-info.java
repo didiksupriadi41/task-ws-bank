a2eb40cdffd89a7458d804c059e4286c60aa75f6
Didik Supriadi <didiksupriadi41@gmail.com>
2019-11-21 18:38:53 +0700

    Add project dependencies and structure

---
+++ src/main/java/org/test/ws/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Bank WS classes is used to serve functions on database.
+ * <p>
+ * These classes contain the service implementation.
+ * </p>
+ *
+ * @since 1.0
+ * @author Didik Supriadi
+ * @version 1.0
+ */
+package org.test.ws;
