38b82b82278bc3ce670a706db6a51210de5c8dc6
Didik Supriadi <didiksupriadi41@gmail.com>
2019-11-21 21:34:15 +0700

    Add options

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 ssh \
+  -o "StrictHostKeyChecking=no" \
   -i $FED_GP2_PEM $FED_GP2_USER@$FED_GP2_IP \
   "rm -rf ws-bank; mkdir ws-bank";
 
 scp \
+  -o "StrictHostKeyChecking=no" \
   -i $FED_GP2_PEM \
   * $FED_GP2_USER@$FED_GP2_IP:/home/$FED_GP2_USER/ws-bank;
 
 ssh \
+  -o "StrictHostKeyChecking=no" \
   -i $FED_GP2_PEM $FED_GP2_USER@$FED_GP2_IP \
   "cd ws-bank; mvn clean tomcat7:redeploy";
