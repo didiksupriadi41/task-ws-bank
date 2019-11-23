ce8d983126024e1109c0b6f483de6166db544136
Johanes <antoniusjohanes99@gmail.com>
2019-11-23 13:05:33 +0700

    fix linting

--- src/main/java/org/test/ws/BankService.java
+++ src/main/java/org/test/ws/BankService.java
@@ -25,7 +25,8 @@ public class BankService {
   /**
    * DB URL.
    */
-  public static final String JDBC_MARIADB_BANK = "jdbc:mariadb://localhost:3306/bank";
+  public static final String JDBC_MARIADB_BANK =
+    "jdbc:mariadb://localhost:3306/bank";
 
   /**
    * DB User.
@@ -94,7 +95,10 @@ public class BankService {
    */
   @WebMethod(operationName = "getFirstNasabah")
   public String getFirstNasabah() throws SQLException {
-    try (Connection conn = DriverManager.getConnection(JDBC_MARIADB_BANK, USER, PASSWORD)) {
+    try (Connection conn = DriverManager.getConnection(
+      JDBC_MARIADB_BANK,
+      USER,
+      PASSWORD)) {
       try (Statement stmt = conn.createStatement()) {
         try (ResultSet rs = stmt.executeQuery("select * from account")) {
           rs.first();
@@ -112,11 +116,17 @@ public class BankService {
    * @throws SQLException Triggered if there are problems with the SQL
    */
   @WebMethod(operationName = "getUserData")
-  public String getUserData(@WebParam(name = "accountNumber") final String accountNumber) throws SQLException {
+  public String getUserData(
+    @WebParam(name = "accountNumber") final String accountNumber)
+    throws SQLException {
     JSONArray userData = new JSONArray();
-    String query = "SELECT * " + "FROM account NATURAL JOIN transaction " + "WHERE account_number = ?;";
+    String query = "SELECT * " + "FROM account NATURAL JOIN transaction "
+      + "WHERE account_number = ?;";
 
-    try (Connection conn = DriverManager.getConnection(JDBC_MARIADB_BANK, USER, PASSWORD)) {
+    try (Connection conn = DriverManager.getConnection(
+      JDBC_MARIADB_BANK,
+      USER,
+      PASSWORD)) {
       try (PreparedStatement stmt = conn.prepareStatement(query)) {
         stmt.setString(Q_PARAM_1, accountNumber);
         try (ResultSet rs = stmt.executeQuery()) {
@@ -144,18 +154,25 @@ public class BankService {
    * @param amount       String of amount.
    * @param startDate    String of start date.
    * @param endDate      String of end date.
-   * @return <code>true</code> if transaction exist, <code>false</code> otherwise.
+   * @return <code>true</code> if transaction exist,
+   *         <code>false</code> otherwise.
    * @throws SQLException Triggered if there are problems with SQL
    */
   @WebMethod(operationName = "checkTransactionBetween")
-  public boolean checkTransactionBetween(@WebParam(name = "linkedNumber") final String linkedNumber,
-      @WebParam(name = "amount") final String amount, @WebParam(name = "startDate") final String startDate,
+  public boolean checkTransactionBetween(
+      @WebParam(name = "linkedNumber") final String linkedNumber,
+      @WebParam(name = "amount") final String amount,
+      @WebParam(name = "startDate") final String startDate,
       @WebParam(name = "endDate") final String endDate) throws SQLException {
 
-    String query = "SELECT * " + "FROM transaction " + "WHERE transaction_time > ? " + "AND transaction_time < ? "
+    String query = "SELECT * " + "FROM transaction "
+        + "WHERE transaction_time > ? " + "AND transaction_time < ? "
         + "AND linked_number = ? " + "AND amount = ? " + "AND type = 'K';";
 
-    try (Connection conn = DriverManager.getConnection(JDBC_MARIADB_BANK, USER, PASSWORD)) {
+    try (Connection conn = DriverManager.getConnection(
+      JDBC_MARIADB_BANK,
+      USER,
+      PASSWORD)) {
       try (PreparedStatement stmt = conn.prepareStatement(query)) {
         stmt.setString(Q_PARAM_1, startDate);
         stmt.setString(Q_PARAM_2, endDate);
@@ -176,12 +193,17 @@ public class BankService {
    * @throws SQLException Triggered if there are problems with SQL
    */
   @WebMethod(operationName = "validateAccountNumber")
-  public boolean validateAccountNumber(@WebParam(name = "accountNumber") final String accountNumber)
+  public boolean validateAccountNumber(
+    @WebParam(name = "accountNumber") final String accountNumber)
       throws SQLException {
 
-    String query = "SELECT * " + "FROM transaction " + "WHERE account_number=? ";
+    String query = "SELECT * " + "FROM transaction "
+      + "WHERE account_number=? ";
 
-    try (Connection conn = DriverManager.getConnection(JDBC_MARIADB_BANK, USER, PASSWORD)) {
+    try (Connection conn = DriverManager.getConnection(
+      JDBC_MARIADB_BANK,
+      USER,
+      PASSWORD)) {
       try (PreparedStatement stmt = conn.prepareStatement(query)) {
         stmt.setString(Q_PARAM_1, accountNumber);
         try (ResultSet rs = stmt.executeQuery()) {
